85179780a41ed8e4b002b938a0077efdad50b289
Giannis Lagodimos <giannislag@proton.me>
2023-12-10 14:54:37 +0200

    use /tmp to save the compiled binaries on linux

--- i2p/run.sh
+++ i2p/run.sh
@@ -1,5 +1,5 @@
 # Linux
-gcc ./main.c -o ./out && chmod a+x ./out && ./out
+gcc ./main.c -o /tmp/out && chmod a+x /tmp/out && /tmp/out
 
 # Compile Windows exe on Linux
 x86_64-w64-mingw32-gcc main.c -o main.exe ./*.dll && wine main.exe
