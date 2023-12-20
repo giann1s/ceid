369b8e9722836c26902b2a06d31b00331a6440d0
Giannis Lagodimos <giannislag@proton.me>
2023-12-20 20:08:51 +0200

    fix: orders-handling crashing when opening non-existent file and some code improvements

--- i2p/orders-handling/main.c
+++ i2p/orders-handling/main.c
@@ -40,7 +40,7 @@ int get_choice();
 Order get_order();
 void display_orders(Order orders[], int num, int status);
 void export_orders(char filename[], Order orders[], int orders_num, int status);
-void import_orders(char filename[], Order orders[], int *orders_num, int status);
+void import_orders(char filename[], Order orders[], int *orders_num);
 void modify_order(Order orders[], int orders_num, int action);
 
 int main() {
@@ -67,7 +67,7 @@ int main() {
                 export_orders("pending_orders.yaml", orders, orders_num, PENDING);
                 break;
             case 5:
-                import_orders("pending_orders.yaml", orders, &orders_num, PENDING);
+                import_orders("pending_orders.yaml", orders, &orders_num);
                 break;
             case 6:
                 modify_order(orders, orders_num, PLACE);
@@ -85,7 +85,7 @@ int main() {
                 export_orders("closed_orders.yaml", orders, orders_num, CLOSED);
                 break;
             case 11:
-                import_orders("closed_orders.yaml", orders, &orders_num, CLOSED);
+                import_orders("closed_orders.yaml", orders, &orders_num);
                 break;
         }
 
@@ -156,9 +156,8 @@ Order get_order() {
 
 void display_orders(Order orders[], int orders_num, int status_idx) {
     int i=0;
-    char initial_price_str[15];
-    char final_price_str[15];
-    char discount_str[8];
+    char initial_price_str[15], final_price_str[15], discount_str[8];
+    Order *order;
 
     if (status_idx >= 0) {  // View specific order
         i = status_idx;
@@ -166,44 +165,50 @@ void display_orders(Order orders[], int orders_num, int status_idx) {
     }
 
     for (; i<orders_num; i++) {
-        sprintf(initial_price_str, "%.2f\u20AC", orders[i].initial_price);
-        sprintf(final_price_str, "%.2f\u20AC", orders[i].initial_price * (1 - orders[i].discount));
-        sprintf(discount_str, "%3.0f%%", orders[i].discount * 100);
+        order = &orders[i];
 
-        if (orders[i].status == PENDING) {
+        sprintf(initial_price_str, "%.2f\u20AC", order->initial_price);
+        sprintf(final_price_str, "%.2f\u20AC", order->initial_price * (1 - order->discount));
+        sprintf(discount_str, "%3.0f%%", order->discount * 100);
+
+        if (order->status == PENDING) {
             strcpy(initial_price_str, "N/A  ");
             strcpy(final_price_str, "N/A  ");
             strcpy(discount_str, "N/A");
         }
-        else if (orders[i].status == READY) {
+        else if (order->status == READY) {
             strcpy(final_price_str, "N/A  ");
             strcpy(discount_str, "N/A");
         }
 
-        if (status_idx == ANY_STATUS || orders[i].status == status_idx || status_idx >= 0)
+        if (status_idx == ANY_STATUS || order->status == status_idx || status_idx >= 0)
             printf("%d %-21s %5d %5d %11s %11s %11s %11s%s %11s%s %4s %8s\n",
                 i+1,
-                orders[i].customer_name,
-                orders[i].small_bottles,
-                orders[i].big_bottles,
-                orders[i].creation_date,
-                orders[i].date,
-                orders[i].execution_date,
-                initial_price_str, (orders[i].status != PENDING) ? "  " : "",
-                final_price_str, (orders[i].status == CLOSED) ? "  " : "",
+                order->customer_name,
+                order->small_bottles,
+                order->big_bottles,
+                order->creation_date,
+                order->date,
+                order->execution_date,
+                initial_price_str, (order->status != PENDING) ? "  " : "",
+                final_price_str, (order->status == CLOSED) ? "  " : "",
                 discount_str,
-                orders[i].status == PENDING ? "PENDING" : (orders[i].status == READY ? "READY" : "CLOSED"));
+                order->status == PENDING ? "PENDING" : (order->status == READY ? "READY" : "CLOSED"));
     }
     printf("\n");
 }
 
 void export_orders(char filename[], Order orders[], int orders_num, int status) {
     int idx;
+    Order *order;
 
     FILE *file_handler = fopen(filename, "w+");
 
     for (idx=0; idx<orders_num; idx++) {
-        if (orders[idx].status == status || status == ANY_STATUS) {
+
+        order = &orders[idx];
+
+        if (order->status == status || status == ANY_STATUS) {
             fprintf(file_handler,
                 "%d:\n"
                 "  status: %d\n"
@@ -216,15 +221,15 @@ void export_orders(char filename[], Order orders[], int orders_num, int status) 
                 "  initial_price: %f\n"
                 "  discount: %f\n",
                 idx,
-                orders[idx].status,
-                orders[idx].customer_name,
-                orders[idx].creation_date,
-                orders[idx].date,
-                orders[idx].execution_date,
-                orders[idx].small_bottles,
-                orders[idx].big_bottles,
-                orders[idx].initial_price,
-                orders[idx].discount);
+                order->status,
+                order->customer_name,
+                order->creation_date,
+                order->date,
+                order->execution_date,
+                order->small_bottles,
+                order->big_bottles,
+                order->initial_price,
+                order->discount);
         }
     }
 
@@ -233,59 +238,69 @@ void export_orders(char filename[], Order orders[], int orders_num, int status) 
     printf("Orders Exported\n\n");
 }
 
-void import_orders(char filename[], Order orders[], int *orders_num, int status) {
+void import_orders(char filename[], Order orders[], int *orders_num) {
     char buffer[MAX_DATA_LINE_LEN];
     char key[20], value[20];
     int indent = 0;
+    Order *order;
 
     FILE *file_handler = fopen(filename, "r");
 
-    while (fgets(buffer, MAX_DATA_LINE_LEN, file_handler) != NULL) {
-        while (buffer[indent] == ' ') {
-            indent++;
-        }
+    if (file_handler != 0) {
+        while (fgets(buffer, MAX_DATA_LINE_LEN, file_handler) != NULL) {
 
-        if (buffer[0] != '\n') {
-            if (indent == 0) (*orders_num)++;
-            else if (indent == 2) {
-                sscanf(buffer, "  %20[^:]: %[a-zA-Z0-9-/. ]", key, value);
+            while (buffer[indent] == ' ') {
+                indent++;
+            }
 
-                if (strcmp(key, "status") == 0) {
-                    sscanf(value, "%d", &orders[*orders_num-1].status);
+            if (buffer[0] != '\n') {
+                if (indent == 0) {
+                    (*orders_num)++;
                 }
-                else if (strcmp(key, "customer_name") == 0) {
-                    sscanf(value, FULL_NAME, &orders[*orders_num-1].customer_name);
-                }
-                else if (strcmp(key, "creation_date") == 0) {
-                    sscanf(value, DATE, &orders[*orders_num-1].creation_date);
-                }
-                else if (strcmp(key, "date") == 0) {
-                    sscanf(value, DATE, &orders[*orders_num-1].date);
-                }
-                else if (strcmp(key, "execution_date") == 0) {
-                    sscanf(value, DATE, &orders[*orders_num-1].execution_date);
-                }
-                else if (strcmp(key, "small_bottles") == 0) {
-                    sscanf(value, "%d", &orders[*orders_num-1].small_bottles);
-                }
-                else if (strcmp(key, "big_bottles") == 0) {
-                    sscanf(value, "%d", &orders[*orders_num-1].big_bottles);
-                }
-                else if (strcmp(key, "initial_price") == 0) {
-                    sscanf(value, "%f", &orders[*orders_num-1].initial_price);
-                }
-                else if (strcmp(key, "discount") == 0) {
-                    sscanf(value, "%f", &orders[*orders_num-1].discount);
+                else if (indent == 2) {
+                    order = &orders[*orders_num-1];
+
+                    sscanf(buffer, "  %20[^:]: %[a-zA-Z0-9-/. ]", key, value);
+
+                    if (strcmp(key, "status") == 0) {
+                        sscanf(value, "%d", &order->status);
+                    }
+                    else if (strcmp(key, "customer_name") == 0) {
+                        sscanf(value, FULL_NAME, &order->customer_name);
+                    }
+                    else if (strcmp(key, "creation_date") == 0) {
+                        sscanf(value, DATE, &order->creation_date);
+                    }
+                    else if (strcmp(key, "date") == 0) {
+                        sscanf(value, DATE, &order->date);
+                    }
+                    else if (strcmp(key, "execution_date") == 0) {
+                        sscanf(value, DATE, &order->execution_date);
+                    }
+                    else if (strcmp(key, "small_bottles") == 0) {
+                        sscanf(value, "%d", &order->small_bottles);
+                    }
+                    else if (strcmp(key, "big_bottles") == 0) {
+                        sscanf(value, "%d", &order->big_bottles);
+                    }
+                    else if (strcmp(key, "initial_price") == 0) {
+                        sscanf(value, "%f", &order->initial_price);
+                    }
+                    else if (strcmp(key, "discount") == 0) {
+                        sscanf(value, "%f", &order->discount);
+                    }
                 }
             }
+
+            indent = 0;
         }
+        fclose(file_handler);
 
-        indent = 0;
+        printf("Orders Imported\n\n");
     }
-
-    fclose(file_handler);
-
-    printf("Orders Imported\n\n");
+    else {
+        printf("%s not found!\n\n", filename);
+    }
 }
 
 void modify_order(Order orders[], int orders_num, int action) {
@@ -297,31 +312,35 @@ void modify_order(Order orders[], int orders_num, int action) {
     // 2. PENDING -> READY          2. READY -> CLOSED
 
     int order_idx = 0, target_status = (action == PLACE) ? PENDING : READY;
+    Order *order;
 
     while(orders[order_idx].status != target_status && order_idx<orders_num) {
         order_idx++;
     }
+    order = &orders[order_idx];
 
     if (order_idx == orders_num) {
         printf("No order found for this action\n");
     }
     else if (action == PLACE) {
-        orders[order_idx].initial_price =
-            orders[order_idx].small_bottles * small_bottle_cost +
-            orders[order_idx].big_bottles * big_bottle_cost;
-        orders[order_idx].status = READY;
+        order->initial_price =
+            order->small_bottles * small_bottle_cost +
+            order->big_bottles * big_bottle_cost;
+        order->status = READY;
 
         display_orders(orders, orders_num, order_idx);
     }
     else if (action == CLOSE) {
-        if (orders[order_idx].initial_price > 600) {
-            orders[order_idx].discount = 0.2;
+        if (order->initial_price > 600) {
+            order->discount = 0.2;
         }
-        else if (orders[order_idx].initial_price > 200 || (orders[order_idx].small_bottles + orders[order_idx].big_bottles) > 3000) {
-            orders[order_idx].discount = 0.08;
+        else if (order->initial_price > 200 ||
+                (order->small_bottles + order->big_bottles) > 3000)
+        {
+            order->discount = 0.08;
         }
 
-        orders[order_idx].status = CLOSED;
+        order->status = CLOSED;
 
         display_orders(orders, orders_num, order_idx);
     }
--- i2p/orders-handling/simple-export-import.c
+++ i2p/orders-handling/simple-export-import.c
@@ -1,20 +1,24 @@
 void export_orders(char filename[], Order orders[], int orders_num, int status) {
     int idx;
+    Order *order;
 
     FILE *file_handler = fopen(filename, "w");
 
     for (idx=0; idx<orders_num; idx++) {
-        if (orders[idx].status == status || status == ANY_STATUS) {
+
+        order = &orders[idx];
+
+        if (order->status == status || status == ANY_STATUS) {
             fprintf(file_handler, "%d;%s;%s;%s;%s;%d;%d;%f;%f\n",
-                orders[idx].status,
-                orders[idx].customer_name,
-                orders[idx].creation_date,
-                orders[idx].date,
-                orders[idx].execution_date,
-                orders[idx].small_bottles,
-                orders[idx].big_bottles,
-                orders[idx].initial_price,
-                orders[idx].discount);
+                order->status,
+                order->customer_name,
+                order->creation_date,
+                order->date,
+                order->execution_date,
+                order->small_bottles,
+                order->big_bottles,
+                order->initial_price,
+                order->discount);
         }
     }
 
@@ -23,24 +27,27 @@ void export_orders(char filename[], Order orders[], int orders_num, int status) 
     printf("Orders Exported\n\n");
 }
 
-void import_orders(char filename[], Order orders[], int *orders_num, int status) {
+void import_orders(char filename[], Order orders[], int *orders_num) {
     char buffer[MAX_DATA_LINE_LEN];
+    Order *order;
 
     FILE *file_handler = fopen(filename, "r");
 
     while (fgets(buffer, MAX_DATA_LINE_LEN, file_handler) != NULL) {
 
+        order = &orders[*orders_num];
+
         sscanf(buffer, "%d;%[a-zA-Z ];%[0-9/];%[0-9/];%[0-9/];%d;%d;%f;%f",
-            &orders[*orders_num].status,
-            orders[*orders_num].customer_name,
-            orders[*orders_num].creation_date,
-            orders[*orders_num].date,
-            &orders[*orders_num].execution_date,
-            &orders[*orders_num].small_bottles,
-            &orders[*orders_num].big_bottles,
-            &orders[*orders_num].initial_price,
-            &orders[*orders_num].discount);
-        
+            &order->status,
+            order->customer_name,
+            order->creation_date,
+            order->date,
+            &order->execution_date,
+            &order->small_bottles,
+            &order->big_bottles,
+            &order->initial_price,
+            &order->discount);
+
         (*orders_num)++;
     }
 
